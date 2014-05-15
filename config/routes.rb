c16cb15250ae523cf4df8d8099cdc41d85138caa
Khoi Pham <khoi@misfit.com>
2014-05-15 12:06:25 +0700

    Add tutorial/index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'tutorial/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
